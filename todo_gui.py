bbe95edb127595e46533de8511b68d2274162386
allisonturnbow <abt3885@mavs.uta.edu>
2026-01-30 17:53:23 -0600

    update gui

--- todo_gui.py
+++ todo_gui.py
@@ -1,6 +1,151 @@
 import tkinter as tk
+import json
+import os
+
+#dictionary style below:
+#{task description, "complete": "True/False" }
+'''
+tasks = [
+    {'task': "first task", 'status': "complete"}
+    
+]
+'''
+root = tk.Tk()
+
+root.title("To-Do List")
+root.geometry("900x600")
+
+try:
+    with open("todo_list.json" , 'r') as file:
+        tasks = json.load(file)
+
+except FileNotFoundError:
+    tasks = []
+
+except json.JSONDecodeError:
+    tasks = []
+
+'''def printtodo():
+    #Display To-Do List
+    #print(clear)
+    print("\n\n\n\n-----------------------------------------------------------------------------------------------")
+    print("To-do List")
+    print("1. add item")
+    print("2. delete item")
+    print("3. view tasks")
+    print("0. exit")
+    print("-----------------------------------------------------------------------------------------------")
+    #print(clear)'''
+    
+    
+def add_task():
+    temptask = input("Task: ")
+    tempstatus = input("Status: ")
+
+    newtask = {"task": temptask, "status": tempstatus}
+    tasks.append(newtask)
+
+    with open("todo_list.json", "w") as file:
+        json.dump(tasks, file, indent = 4)
+
+def del_task():
+    del_val = int(input(f"which task would you like to delete? 1 - {len(tasks)}: "))
+
+    tasks.pop(del_val - 1)
+
+    with open("todo_list.json", "w") as file:
+        json.dump(tasks, file, indent = 4)
+
+def view_list():
+    for index, task in enumerate(tasks):
+        temp_label = tk.Label(root, text = f"{index + 1}. {task['task']} -------- {task['status']}").grid()
 
 
+
+#add button for adding a task to the list
+add_button = tk.Button(root, text = "add task", command=add_task).grid(row = 0, column = 0)
+del_button = tk.Button(root, text = "update", command = del_task).grid(row = 1, column = 0)
+label1 = tk.Label(root, text = "To do List items:", font = ("Arial", 14, "bold")).grid(row = 4, column = 0, columnspan = 3)
+view_list()
+
+
+
+
+
+
+'''
+
+def todo():
+    while True:
+        #file handling'
+        try:
+            with open("todo_list.json" , 'r') as file:
+                tasks = json.load(file)
+
+        except FileNotFoundError:
+            tasks = []
+
+        except json.JSONDecodeError:
+            tasks = []
+            
+        printtodo()
+
+        choice = input("\n\n\nEnter your choice (0-3)\n\n")
+
+
+        if choice == '0' :
+            print("Done with Todo List...") 
+            break
+
+        elif choice == '1' :
+            temptask = input("Task: ")
+            tempstatus = input("Status: ")
+
+            newtask = {"task": temptask, "status": tempstatus}
+            tasks.append(newtask)
+
+            with open("todo_list.json", "w") as file:
+                json.dump(tasks, file, indent = 4)
+            
+
+        elif choice == '2' :
+            del_val = int(input(f"which task would you like to delete? 1 - {len(tasks)}: "))
+
+            tasks.pop(del_val - 1)
+
+            with open("todo_list.json", "w") as file:
+                json.dump(tasks, file, indent = 4)
+            
+
+
+
+        elif choice == '3' :
+            print("viewing to-do list...\n") 
+            for index, task in enumerate(tasks):
+                print(f"{index + 1}. {task['task']} -------- {task['status']}")
+
+        else:
+            print("invalid input")
+
+    
+        
+
+
+todo()
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+'''
 root = tk.Tk()
 
 root.title("To-Do List")
@@ -53,13 +198,10 @@ def print_list(temp_list):
 
 
 #add button for adding a task to the list
-add_button = tk.Button(root, text = "add task", command=add_task)
-update_button = tk.Button(root, text = "update", command = update)
-label1 = tk.Label(root, text = "To do List items:", font = ("Arial", 14, "bold"))
+add_button = tk.Button(root, text = "add task", command=add_task).grid(row = 0, column = 0)
+update_button = tk.Button(root, text = "update", command = update).grid(row = 1, column = 0)
+label1 = tk.Label(root, text = "To do List items:", font = ("Arial", 14, "bold")).grid(row = 4, column = 0, columnspan = 3)
 
-add_button.grid(row = 0, column = 0)
-update_button.grid(row = 1, column = 0)
-label1.grid(row = 4, column = 0, columnspan = 3)
 
 
 
@@ -71,3 +213,5 @@ update_entry_box.grid(row = 1, column = 1, columnspan = 2)
 
 
 root.mainloop()
+
+'''
